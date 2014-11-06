cf36999dee3930e289de0948405edca95ebfae4c
River <riverchiu0629@gmail.com>
2014-11-06 12:31:16 +0800

    增加新增
    Signed-off-by: River <riverchiu0629@gmail.com>

--- test.php
+++ test.php
@@ -1,24 +1,20 @@
-<form>
-<a href="test.php?action=insert,a="+>存入</a>
-<a href="test.php">取消</a>
-<br/>
-<input type="text" id="lab1" value="user"/>
-<br/>
-<input type="text" name="lab2" value="10"/>
-<br/>
-<input type="text" name="lab3" value="20"/>
+<form action="test.php" method="post">
+輸入:<br/>
+<input type="text" name="text_name"/><br/>
+<input type="text" name="text_age"/><br/>
+<input type="text" name="text_num"/><br/>
+<input type="submit" value="clickme"/>
 </form>
 <?php
-if(isset($_GET["action"]))
+if(isset($_POST["text_name"]))
 {
-	if ($_GET["action"]=="insert")
-	{
-	echo $lab1;
-	//$link = mysql_connect("127.0.0.1", "root", "");
-	//mysql_select_db("test01_data", $link);
-	//$result = mysql_query("INSERT INTO `person`(`name`, `age`, `num`) VALUES ('".lab1.value."',".lab2.value.",".lab3.value.")", $link);
-	}
-	
+	//echo $_POST["text_name"].$_POST["text_age"].$_POST["text_num"];
+	$link = mysql_connect("127.0.0.1", "root", "");
+	mysql_select_db("test01_data", $link);
+	$result = mysql_query("INSERT INTO `person`(`name`, `age`, `num`) VALUES ('".$_POST["text_name"]."',".$_POST["text_age"].",".$_POST["text_num"].")", $link);
+	$_POST["text_name"]=null;
+	$_POST["text_age"]=null;
+	$_POST["text_num"]=null;
 }
 
 $link = mysql_connect("127.0.0.1", "root", "");
