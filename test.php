d85fdfc2f54870af844f1f6c6f99500bb735919e
River <riverchiu0629@gmail.com>
2014-11-10 17:15:03 +0800

    testuse
    Signed-off-by: River <riverchiu0629@gmail.com>

--- test.php
+++ test.php
@@ -8,6 +8,7 @@
 <?php
 if(isset($_POST["text_name"]))
 {
+//又增加
 	//echo $_POST["text_name"].$_POST["text_age"].$_POST["text_num"];
 	$link = mysql_connect("127.0.0.1", "root", "");
 	mysql_select_db("test01_data", $link);
